49fcc935e460125507a46951f7c588a74b7743e6
Nicolas Deslandes <deslandes.nicolas@gmail.com>
2012-11-22 22:05:33 -0500

    You are now 122/290 koans and 15/36 lessons away from reaching enlightenment

--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -19,7 +19,20 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    if a == b == c == 0:
+        raise TriangleError
+    if a < 0 or b < 0 or c < 0:
+        raise TriangleError
+    if a + b <= c or b + c <= a or a + c <= b:
+        raise TriangleError
+
+    if a == b == c:
+        return 'equilateral'
+    else:
+        if a == b or b == c or a == c:
+            return 'isosceles'
+        else:
+            return 'scalene'
 
 
 # Error class used in part 2.  No need to change this code.
